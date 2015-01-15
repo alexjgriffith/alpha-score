ac1977249b60e067c05d2774afcbd393b21603ab
alexjgriffith <griffitaj@gmail.com>
2015-01-15 12:48:27 -0500

    added r scipts.

---
+++ scripts/a-score.r
@@ -0,0 +1,20 @@
+#!/data/binaries/R-3.1/bin/Rscript
+library('getopt')
+source("pcaAnalysis.r")
+
+spec = matrix(c(
+    'fileLocation','i', 1,"character",
+    'catagories','c', 1,"character",
+    'cols','b', 1,"character"
+    ),byrow=TRUE,ncol=4)
+args=getopt(spec)
+
+root_file<-args$fileLocation
+cat_file<-args$catagories
+
+values<-loadData(rootFile)
+cats<-t(read.table(catFile))
+data<-values$data
+normData<-standPCAPrep(data,"colQn")
+pcs<-prcomp(t(normData))
+write.table(pcs$rotation[as.numeric(strsplit(args$cols,",")[[1]])],stdout(),col.names=FALSE,row.names=FALSE,quote=FALSE,sep="\t")
